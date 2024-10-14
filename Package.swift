bec5ed1bbe761d681ce489aec9499faabada6dfc
Emin Dağ <eemindag@gmail.com>
2024-10-14 23:40:04 +0300

    swift-argument-parser added as dependency

--- Package.swift
+++ Package.swift
@@ -5,10 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "appgalleryconnect",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "appgalleryconnect"),
+            name: "appgalleryconnect", dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
     ]
 )
